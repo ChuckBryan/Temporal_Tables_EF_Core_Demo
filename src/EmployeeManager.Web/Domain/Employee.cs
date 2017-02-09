6c625e026edb162d1f3c0e6f269835c66198fab3
Chuck Bryan <swampyfox@gmail.com>
2017-02-09 06:51:23 -0500

    Adding Initial Code for Demo

---
+++ src/EmployeeManager.Web/Domain/Employee.cs
@@ -0,0 +1,50 @@
+﻿namespace EmployeeManager.Web.Domain
+{
+    using EnsureThat;
+
+    public enum Department
+    {
+        Executive,
+        Engineering,
+        Support
+    }
+
+    public class Employee
+    {
+        public Employee(string firstName, string lastName, Department department)
+        {
+            Ensure.That(() => firstName).IsNotNullOrWhiteSpace();
+            Ensure.That(() => lastName).IsNotNullOrWhiteSpace();
+
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+        }
+
+        private Employee()
+        {
+        }
+
+        public int Id { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public Department Department { get; private set; }
+
+        public void ChangeEmployeeDepartment(Department newDepartment)
+        {
+            Department = newDepartment;
+        }
+
+        public void UpdateEmployeeName(string firstName, string lastName)
+        {
+            Ensure.That(() => firstName).IsNotNullOrWhiteSpace();
+            Ensure.That(() => lastName).IsNotNullOrWhiteSpace();
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
